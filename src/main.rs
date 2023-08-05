f8354a32e91a035752862b03f4af6f1dfc3f4677
hagaitski <i.haga629@gmail.com>
2023-08-05 13:18:12 +0900

    Resultに変更

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-use wasm_bindgen::JsCast;
-use web_sys::console;
+use wasm_bindgen::{JsCast, JsValue};
+use web_sys::{console, window, HtmlCanvasElement, CanvasRenderingContext2d};
 
-fn main() {
+fn main() -> Result<(), JsValue> {
     console::log_1(&"Hello world!".into());
     // body配下にcanvasを追加
-    let document = web_sys::window().unwrap().document().unwrap();
-    let body = document.body().unwrap();
-    let canvas = document.create_element("canvas").unwrap();
-    body.append_child(&canvas).unwrap();
+    let window = window().ok_or_else(|| JsValue::from_str("No global `window` exists"))?;
+    let document = window.document().ok_or_else(|| JsValue::from_str("The window does not have a document"))?;
+    let body = document.body().ok_or_else(|| JsValue::from_str("The document does not have a body"))?;
+    let canvas = document.create_element("canvas")?;
+    body.append_child(&canvas)?;
+
     // canvasを初期化
-    let canvas = canvas.dyn_into::<web_sys::HtmlCanvasElement>().unwrap();
-    let context = canvas.get_context("2d").unwrap().unwrap().dyn_into::<web_sys::CanvasRenderingContext2d>().unwrap();
+    let canvas: HtmlCanvasElement = canvas.dyn_into()?;
+    let context = canvas.get_context("2d")?.ok_or_else(|| JsValue::from_str("The canvas does not have a 2d context"))?;
+    let context: CanvasRenderingContext2d = context.dyn_into()?;
+
     // canvasを塗りつぶす
     context.begin_path();
     context.rect(0.0, 0.0, 100.0, 100.0);
     context.fill();
+
+    Ok(())
 }
